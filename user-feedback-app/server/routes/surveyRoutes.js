a527fbfe924be0c40007dfb11693cb3b9dbedab0
ananth <ananth.bhat@team.telstra.com>
2020-05-09 10:36:31 +0530

    create a new survey in the route handler

--- user-feedback-app/server/routes/surveyRoutes.js
+++ user-feedback-app/server/routes/surveyRoutes.js
@@ -1,8 +1,21 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+
+const Survey = mongoose.model('surveys');
 
 function surveyRoutes(app) {
-    app.post('/api/surveys', requireLogin, (req, res) => { //make sure user is logged in and he has enough credits to create surveys
-
+    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => { //make sure user is logged in and he has enough credits to create surveys
+        const { title, subject, body, recipients } = req.body();
+        survey = new Survey({
+            title,
+            subject,
+            body,
+            // recipients: recipients.split(',').map(email => { return { email: email } })
+            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+            _user: req.user.id,
+            dateSent: Date.now()
+        });
     });
 }
 module.exports = surveyRoutes;

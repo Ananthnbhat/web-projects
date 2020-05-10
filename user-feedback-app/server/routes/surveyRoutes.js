baa2cd38dc3e75df8f269717f0590a2c132b2b7a
ananth <ananth.bhat@team.telstra.com>
2020-05-10 18:20:50 +0530

    surveys backend almost completed

--- user-feedback-app/server/routes/surveyRoutes.js
+++ user-feedback-app/server/routes/surveyRoutes.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
 function surveyRoutes(app) {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => { //make sure user is logged in and he has enough credits to create surveys
+
+    app.get('/api/surveys/thanks', (req, res) => {
+        res.send("Thank you for voting!")
+    });
+
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => { //make sure user is logged in and he has enough credits to create surveys
         const { title, subject, body, recipients } = req.body();
         survey = new Survey({
             title,
@@ -16,6 +23,18 @@ function surveyRoutes(app) {
             _user: req.user.id,
             dateSent: Date.now()
         });
+        //place to send an email
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+        try {
+            await mailer.send();
+
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+            res.send(user)
+        } catch (err) {
+            res.status(422).send(err);
+        }
     });
 }
 module.exports = surveyRoutes;

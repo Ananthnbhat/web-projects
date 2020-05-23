c65157af50f7b56cba004b8ad8a9f96a13100711
ananth <ananth.bhat@team.telstra.com>
2020-05-23 14:59:47 +0530

    probably last commit

--- user-feedback-app/server/client/src/components/Dashboard.js
+++ user-feedback-app/server/client/src/components/Dashboard.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SurveyList from './surveys/SurveyList';
 import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
         <div>
-            Dashboard here
+            <SurveyList />
             <div className="fixed-action-btn">
                 <Link to="/surveys/new" className="btn-floating btn-large red">
                     <i className="material-icons">add</i>
--- user-feedback-app/server/routes/surveyRoutes.js
+++ user-feedback-app/server/routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -8,14 +11,48 @@ const Survey = mongoose.model('surveys');
 
 function surveyRoutes(app) {
 
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys', requireLogin, async (req, res) => {
+        const surveys = await Survey.find({ _user: req.user.id })
+            .select({ recipients: false });
+
+        res.send(surveys);
+    });
+
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send("Thank you for voting!")
     });
 
-    app.post('/api/surveys/webhooks', (req, res)=>{
-        console.log(req.body);
+    app.post('/api/surveys/webhooks', (req, res) => {
+
+        const p = new Path('/api/surveys/:surveyId/:choice');
+
+        _.chain(req.body)
+            .map(({ email, url }) => {
+                const match = p.test(new URL(url).pathname);
+                if (match) {
+                    return { email, surveyId: match.surveyId, choice: match.choice };
+                }
+            })
+            .compact()  //remove undefined elements in the array
+            .uniqBy('email', 'surveyId')  //removes duplicate elements in array
+            .each(({ surveyId, email, choice }) => {
+                Survey.updateOne(  //look at the Survey collection, find and update one record
+                    {
+                        _id: surveyId,
+                        recipients: {
+                            $elemMatch: { email: email, responded: false }
+                        }
+                    },
+                    {
+                        $inc: { [choice]: 1 },  // yes or no is written as [choice]
+                        $set: { 'recipients.$.responded': true }, //here $ means the record selected using $elemMatch above
+                        lastResponded: new Date()
+                    }).exec();
+            })
+            .value();
+
         res.send({});
-    })
+    });
 
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => { //make sure user is logged in and he has enough credits to create surveys
         const { title, subject, body, recipients } = req.body;

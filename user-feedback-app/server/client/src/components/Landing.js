8a3faffe68a2b7cd6502679487ad0797c071ee1e
Ananthnbhat <ananthnbhat2@gmail.com>
2021-09-20 09:38:38 +0530

    update credit card instructions

--- user-feedback-app/server/client/src/components/Landing.js
+++ user-feedback-app/server/client/src/components/Landing.js
@@ -6,6 +6,11 @@ const Landing = () => {
         <div style={{ textAlign: 'center' }}>
             <h2>User-feedback landing page</h2>
             Collect feedback from users
+            <blockquote>Note: for testing purposes, please use default stripe credit card details</blockquote>
+            <p>Any mail id is fine</p>
+            <p>Card number: 4242 4242 4242 4242</p>
+            <p>Any 3 digit CVC</p>
+            <p>Any future data</p>
         </div>
     );
 }

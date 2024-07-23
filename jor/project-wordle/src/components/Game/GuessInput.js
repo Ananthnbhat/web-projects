6603a2a3e863d6193ae4dd93ba4d92a9f60eb162
anant <ananthnbhat2@gmail.com>
2024-07-23 18:34:37 +1000

    minor refactors

--- jor/project-wordle/src/components/Game/GuessInput.js
+++ jor/project-wordle/src/components/Game/GuessInput.js
@@ -3,7 +3,7 @@ import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 const GuessInput = ({ prevGuesses, setGuesses, answer }) => {
     const [guess, setGuess] = useState('');
-    const [failStatus, setFailStatus] = useState(false);
+    const [gameStatus, setGameStatus] = useState('running');
     const [statusBanner, setStatusBanner] = useState(null);
 
     const ALLOWED_GUESSES = NUM_OF_GUESSES_ALLOWED - 1;
@@ -15,23 +15,24 @@ const GuessInput = ({ prevGuesses, setGuesses, answer }) => {
             setGuesses([...prevGuesses, guess])
         }
         if (guess === answer) {
-            setFailStatus(true);
+            setGameStatus('won');
             setStatusBanner(
                 <div className="happy banner">
                     <p>
-                        <strong>Congratulations!</strong> Got it in
+                        <strong>Congratulations!</strong> Got it in {' '}
                         <strong>{prevGuesses.length + 1} guesses</strong>.
                     </p>
                 </div>
             )
         } else if (guess != answer && prevGuesses.length === ALLOWED_GUESSES - 1) {
-            setFailStatus(true);
+            setGameStatus('lost');
             setStatusBanner(
                 <div className="sad banner">
                     <p>Sorry, the correct answer is <strong>{answer}</strong>.</p>
                 </div>
             )
         }
+        setGuess('')
     }
     return <>
         <form className='guess-input-wrapper' onSubmit={handleSubmit}>
@@ -42,9 +43,10 @@ const GuessInput = ({ prevGuesses, setGuesses, answer }) => {
                 value={guess}
                 pattern={`[a-zA-Z]{${ALLOWED_GUESSES}}`}
                 title={`${ALLOWED_GUESSES} letter word`}
-                disabled={failStatus}
+                disabled={gameStatus != 'running'}
                 onChange={(e) => setGuess(e.target.value.toUpperCase())} />
         </form>
+        {/* the banner should in its own component & a child of <Game /> component */}
         {statusBanner}
     </>;
 }
